194988cb7243a36f67bac0ade5789231de5e7bed
fnacarel <fnacarel@student.42sp.org.br>
2022-09-06 19:24:45 +0200

    ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,19 +6,12 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 21:06:05 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/09/01 17:52:31 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/09/06 19:00:05 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
 {
-	size_t	i;
-
-	i = 0;
-	while (i < n)
-	{
-		*((char *) s + i) = '\0';
-		i++;
-	}
+	ft_memset(s, 0, n);
 }
