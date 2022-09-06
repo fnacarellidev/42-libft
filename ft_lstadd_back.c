df9809ef493b9b26c360c43e006f2142634c98c8
fnacarel <fnacarel@student.42sp.org.br>
2022-09-06 20:04:16 +0200

    ft_lstadd_back.c

---
+++ ft_lstadd_back.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/06 18:06:26 by fnacarel          #+#    #+#             */
+/*   Updated: 2022/09/06 19:28:10 by fnacarel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst);
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last_node;
+
+	if (lst && new)
+	{
+		if (*lst == NULL)
+			*lst = new;
+		else
+		{
+			last_node = ft_lstlast(*lst);
+			last_node -> next = new;
+		}
+	}
+}
