c626ba9831b9721219fdfadbe60723265697bbf0
fnacarel <fnacarel@student.42sp.org.br>
2022-09-07 15:59:40 +0200

    remove function del

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,17 +6,10 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 20:07:43 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/09/06 21:43:43 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/09/06 23:19:52 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-
-void	ft_delcontent(void *content);
-
-void	ft_delcontent(void *content)
-{
-	free(content);
-}
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
