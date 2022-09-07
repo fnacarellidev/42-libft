07148634460e1190d2f9cce343ecec711228063d
fnacarel <fnacarel@student.42sp.org.br>
2022-09-07 16:37:08 +0200

    add null function address protection

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 22:42:28 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/09/07 15:58:30 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/09/07 16:35:40 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -16,12 +16,15 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	t_list	*store_previous_node;
 
 	store_previous_node = *lst;
-	while (*lst != 0 && store_previous_node != 0)
+	if (del)
 	{
-		store_previous_node = *lst;
-		(*lst) = (*lst) -> next;
-		del(store_previous_node -> content);
-		free(store_previous_node);
+		while (*lst != 0 && store_previous_node != 0)
+		{
+			store_previous_node = *lst;
+			(*lst) = (*lst) -> next;
+			del(store_previous_node -> content);
+			free(store_previous_node);
+		}
+		lst = NULL;
 	}
-	lst = NULL;
 }
--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,15 +6,16 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 20:07:43 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/09/06 23:19:52 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/09/07 16:36:25 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	if (!lst || !del)
-		return ;
-	del(lst->content);
-	free(lst);
+	if (lst && del)
+	{
+		del(lst->content);
+		free(lst);
+	}
 }
