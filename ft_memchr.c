7267f396f00a5b3dd194f580ae9028b9ac181161
fnacarel <fnacarel@student.42sp.org.br>
2022-09-07 21:00:54 +0200

    refactor and fix for tripoulli

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,24 +6,21 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 03:20:48 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/08/31 17:19:31 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/09/07 20:59:44 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t		i;
-	const char	*first_occurence_address;
+	size_t	i;
 
-	first_occurence_address = 0;
 	i = 0;
-	c = (unsigned char) c;
-	while ((*((char *) s) + i) && i < n)
+	while (i++ < n)
 	{
-		if (*(((unsigned char *) s) + i) == c)
-			return ((void *) s + i);
-		i++;
+		if (*((unsigned char *) s) == (unsigned char) c)
+			return ((void *) s);
+		s++;
 	}
-	return ((void *) first_occurence_address);
+	return (NULL);
 }
