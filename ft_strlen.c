f89582c1e89b2875cbc68daf021e9790f9db9129
fnacarel <fnacarel@student.42sp.org.br>
2022-09-08 20:35:35 +0200

    ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 19:52:12 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/08/29 19:53:22 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/09/08 20:34:37 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -16,10 +16,7 @@ size_t	ft_strlen(const char *s)
 	int	count;
 
 	count = 0;
-	while (*s)
-	{
-		s++;
+	while (*(s + count))
 		count++;
-	}
 	return (count);
 }
