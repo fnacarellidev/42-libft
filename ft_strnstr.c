5ae4c9739eea91cd9a33e49dd0d69570c89e39e1
fnacarel <fnacarel@student.42sp.org.br>
2022-09-08 14:21:38 +0200

    fix for tripouille

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 19:59:17 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/08/31 20:49:24 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/09/08 14:19:10 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -15,21 +15,20 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	char	*ptr;
 
 	i = 0;
-	ptr = 0;
-	if (*little == '\0')
+	if (!*little)
 		return ((char *) big);
 	while (*(big + i) && i < len)
 	{
 		if (*(big + i) == *(little + 0))
 		{
 			j = 0;
-			while (*(big + i + j) == *(little + j))
+			while (*(big + i + j) == *(little + j) && \
+					(*(big + i + j) && *(little + j)))
 			{
-				if (i + j > len)
-					return (ptr);
+				if (i + j >= len)
+					return (NULL);
 				j++;
 			}
 			if (j == ft_strlen(little))
@@ -37,5 +36,5 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 		}
 		i++;
 	}
-	return (ptr);
+	return (NULL);
 }
