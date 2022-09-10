2ed698d481e1dd2f56b08f64da4d84e3229b9af5
fnacarel <fnacarel@student.42sp.org.br>
2022-09-10 16:21:17 +0200

    ft_substr fix and refactoring

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 09:23:27 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/09/02 09:55:54 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/09/08 23:19:39 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,14 +14,21 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
-	int		fullbuffer;
+	int		memory_allocated;
 
-	if (start > ft_strlen(s))
+	if (start > len)
 		len = 0;
-	fullbuffer = (len + 1);
-	substr = (char *) malloc(sizeof(*substr) * fullbuffer);
-	if (!substr)
-		return (NULL);
-	ft_strlcpy(substr, (s + start), fullbuffer);
+	if (start > ft_strlen(s))
+	{
+		substr = (char *) malloc(1);
+		*substr = 0;
+		return (substr);
+	}
+	else if (ft_strlen(s) > len)
+		memory_allocated = len + 1;
+	else
+		memory_allocated = ft_strlen(s) - start + 1;
+	substr = (char *) malloc(sizeof(char) * memory_allocated);
+	ft_strlcpy(substr, s + start, memory_allocated);
 	return (substr);
 }
