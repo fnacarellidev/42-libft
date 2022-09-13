82b5d6647ca2fdc33a7b1fc58b1dc83cdc39367f
fnacarel <fnacarel@student.42sp.org.br>
2022-09-13 23:40:32 +0200

    change ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,25 +6,21 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 02:14:05 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/09/07 20:33:42 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/09/13 23:39:55 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int			i;
-	const char	*first_occurence;
+	int	index;
 
-	i = 0;
-	first_occurence = 0;
-	while (*(s + i))
+	index = ft_strlen(s);
+	while (index >= 0)
 	{
-		if (*(s + i) == (unsigned char) c)
-			first_occurence = (s + i);
-		i++;
+		if (*(s + index) == (char) c)
+			return ((char *)(s + index));
+		index--;
 	}
-	if (c == 0)
-		first_occurence = (s + ft_strlen(s));
-	return ((char *) first_occurence);
+	return (NULL);
 }
